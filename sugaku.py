007240b95398710d1e65c8954d454753bb949e9d
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2022-10-25 17:08:12 +0200

    sugaku module

---
+++ sugaku.py
@@ -0,0 +1,60 @@
+import random
+
+debug = False
+
+num = ["1","2","3","4","5","6", "7","8","9"]
+operators = ["+","-","/","*"]
+
+finalN = []
+finalC = []
+
+
+def max_digit_length(value):
+    return value
+
+    
+def numbers(): 
+    global operators
+    max_len = max_digit_length()
+
+    for i in range(1, max_len+1):
+        n = random.choice(num)
+        finalN.append(n)
+
+    if debug == True:
+        print(finalN)
+
+    x = ''.join(finalN)
+    return x
+
+
+def oper(mode:str):
+    numbers()
+    global operators
+    max_len = max_digit_length()
+
+
+    for i in range(1, max_len+1): # Loop
+        op = random.choice(operators)
+        finalN.append(op) # Adding operators, calling back numbers() to get the next one.
+        numbers()
+
+    y1 = ''.join(finalN) # Final calculation in str
+    y = eval(y1) # Resolve
+    y = str(y)
+
+    if mode == None:
+        return (y1,"=",y)
+    if mode == 'no-res':
+        return y1
+    if mode == 'res':
+        return y
+
+def convert(word): # Converting oper() to a string without ( , ) ' symbols 
+    convert = ''
+    for char in word:
+        if char not in ('(',')', "'", ','):
+            convert += char
+    
+    wordc = convert
+    return wordc
